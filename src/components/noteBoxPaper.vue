3c6cbdb7b1a49ac42f5d03c6a58d5d1a5ba25359
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-13 15:05:49 +0100

    Add components derictory , create noteBoxPaper component

---
+++ src/components/noteBoxPaper.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    data(){
+        return {
+            count: 4
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="notebook-paper">
+        <header>
+            <h1>Fancy Title</h1>
+        </header>
+        <div id="content">
+            <div class="hipsum">
+            <p>Hello amigos</p>
+            <input type="checkbox" id="input">
+            <label for="input">Can You Add this? </label>
+            <br>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+/*  */
+</style>
